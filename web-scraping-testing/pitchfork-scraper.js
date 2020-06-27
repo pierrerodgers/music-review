8c7216fbcbb816da66efabfc1f4a506b59c39131
Pierre Rodgers <pierre.rodgers.1@gmail.com>
2020-06-27 17:02:10 +1000

    Update pitchfork-scraper.js
    added function getPitchforkScore, uses artist name and album title to fetch html for review

--- web-scraping-testing/pitchfork-scraper.js
+++ web-scraping-testing/pitchfork-scraper.js
@@ -1,8 +1,24 @@
 const axios = require('axios');
+const cheerio = require('cheerio')
 
-async function getLink(url) {
+async function getHtml(url) {
     const response = await axios.get(url);
     console.log(response);
 }
 
-getLink('http://www.pitchfork.com/').catch(error => {console.log(error)})
+async function getPitchforkScore(artist, title) {
+    let artistString = artist.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    artistString = artistString.replace(/ /g, '-').toLowerCase();
+    console.log(artistString)
+
+    let titleString = title.replace(/[.,\/#!$'%\^&\*;:{}=\-_`~()]/g,"");
+    titleString = titleString.replace(/ /g, '-').toLowerCase();
+    console.log(titleString);
+
+    const reviewUrl = `http://www.pitchfork.com/reviews/albums/${artistString}-${titleString}`;
+    
+    await getHtml(reviewUrl).catch(error => console.log(error));
+
+
+}
+getPitchforkScore('Charli xcx', "how i'm feeling now");
