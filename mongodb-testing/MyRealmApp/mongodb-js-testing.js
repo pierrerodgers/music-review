8d760b1ccb71bccacb71dda11acbc07c730d0b86
Pierre Rodgers <pierre.rodgers.1@gmail.com>
2020-06-27 11:09:32 +1000

    Update mongodb-js-testing.js
    testing mongodb realms in js

--- mongodb-testing/MyRealmApp/mongodb-js-testing.js
+++ mongodb-testing/MyRealmApp/mongodb-js-testing.js
@@ -10,8 +10,52 @@ const TaskModel = {
     name: "Task",
     primaryKey: "_id",
     properties: {
-      _id: "object id?",
-      name: "string",
-      description: "string",
+        _id: "object id?",
+        name: "string",
+        description: "string",
     },
-  };
+};
+
+async function run() {
+    let user;
+    let realm;
+    let tasks;
+
+    try {
+        const app = new Realm.App(appConfig);
+        console.log('app opened')
+        const credentials = Realm.Credentials.anonymous();
+
+        user = await app.logIn(credentials);
+        console.log('user logged in')
+        const config = {
+            schema: [TaskModel],
+            sync: {
+                user: user,
+                partitionValue: 'tasks',
+            },
+        };
+
+        realm = await Realm.open(config);
+
+        
+
+        console.log(`Logged in with the user: ${user.identity}`);
+
+        realm.write( () => {
+            realm.create("Task", {
+                name: "Buy milk, eggs, and bread",
+                status: "open"
+            });
+        });
+
+
+
+    } finally {
+        user.logOut();
+        realm.close();
+    }
+
+}
+
+run().catch(error => console.log(error));
