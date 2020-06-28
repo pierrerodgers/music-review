fd962f3f864d7200d04df356f71e945cfce970fc
Pierre Rodgers <pierre.rodgers.1@gmail.com>
2020-06-28 19:48:47 +1000

    Update aoty-scraper.js
    artist input from argv, now finds reviewer, score and date

--- web-scraping-testing/aoty-scraper.js
+++ web-scraping-testing/aoty-scraper.js
@@ -10,8 +10,7 @@ async function urlToCheerio(url) {
 
 async function getAlbumLink(artist, title) {
     // Search page by album title, match to artist by filtering html
-    const url = `https://www.albumoftheyear.org/search/albums/?q=${title.replace(/ /g, '%20')}`;
-    console.log(url);
+    const url = `https://www.albumoftheyear.org/search/albums/?q=${title.replace(/ /g, '%20').toLowerCase()}`;
     let $ = await urlToCheerio(url);
 
     const resultsArr = $('.albumBlock .albumTitle');
@@ -31,8 +30,31 @@ async function getAlbumLink(artist, title) {
 
 async function getAotyScores(artist, title) {
     try{
+        console.log(artist);
+        console.log(title);
         let albumLink = await getAlbumLink(artist, title);
-        console.log(albumLink);
+        
+        const $ = await urlToCheerio(albumLink);
+
+        // Reviews stored in divs: id=criticReviewContainer and #criticReviewContainer child #moreCricitReviews (YES TYPO)
+        // Review class: .albumReviewRow
+
+        
+        const reviewerArr = $('.albumReviewHeader > a > span');
+        const scoresArr = $('.albumReviewRating > span');
+        const dateArr = $('.albumReviewRow > [itemprop="dateCreated"]')
+
+        reviewerArr.map( (index, element) => {
+            console.log(element.firstChild.data);
+            console.log(scoresArr[index].firstChild.data);
+            console.log(dateArr[index].attribs.content);
+        });
+    
+       
+    
+    
+    
+    
     }
     catch {
         console.log('error finding album');
@@ -40,14 +62,18 @@ async function getAotyScores(artist, title) {
     
 
 }
-
+if (process.argv.length != 4) console.log('Usage: node aoty-scraper.js "<Artist Name>" "<Album Title>"');
+else {
+    getAotyScores(process.argv[2], process.argv[3]);
+}
 testingArray = [['Charli XCX',  "how i'm feeling now"], ['Khruangbin', 'Mordechai'], 
 ['HAIM', 'Women In Music Pt. III'], ['Weyes Blood', 'Titanic Rising'], ['Perfume Genius', 'Set my heart on fire immediately'], 
 ['Phoebe Bridgers', 'Punisher'], ['Lady Gaga', 'Chromatica'], ['Yves Tumor', 'Heaven to a tortured mind'], 
 ['Moses Sumney', 'grae'], ['Rina Sawayama', 'Sawayama'], ['TOPS', 'I feel alive'],
 ['The 1975', 'Notes on a conditional form']];
 
-testingArray.map( async album =>  {
-    getAotyScores(album[0], album[1]);
+/*testingArray.map( async album =>  {
+    await getAotyScores(album[0], album[1]);
+    console.log(album);
 
-})
+})*/
