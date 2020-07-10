3568e6c0c78754822bd34d90fe604dc2a513c489
Pierre Rodgers <pierre.rodgers.1@gmail.com>
2020-07-10 17:51:20 +1000

    Update apple-music-testing.js
    added error handling

--- web-scraping-testing/metadata-scraping/apple-music-testing.js
+++ web-scraping-testing/metadata-scraping/apple-music-testing.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const fs = require('fs');
 const jwt = require("jsonwebtoken");
+var errors = fs.createWriteStream("apple-music-errors.txt", {flags:'a'});
+
 
 const privateKey = fs.readFileSync("AuthKey_KDNARVQ7Z5.p8").toString(); //your MusicKit private key
 const jwtToken = jwt.sign({}, privateKey, {
@@ -14,23 +16,24 @@ const jwtToken = jwt.sign({}, privateKey, {
 });
 
 async function getAlbumReleaseDate(artist, name) {
-    artist = artist.replace(/ /g, '+');
-    name = name.replace(/ /g, '+');
-    let search = `${artist}+${name}`;
-
-    let response = await axios.get(`https://api.music.apple.com/v1/catalog/us/search?term=${search}&limit=3`, {headers: {Authorization: `Bearer ${jwtToken}`}});
-    //console.log(response.data);
-    let album = response.data.results.albums.data[0];
+    // Create search term ( i.e. /search?Charli+XCX+how+i'm+feeling+now)
+    let search = `${artist.replace(/ /g, '+')}+${name.replace(/ /g, '+')}`;
+    
+    // Get API response using token and get first result
+    let response = await axios.get(`https://api.music.apple.com/v1/catalog/us/search?term=${search}&limit=5`, {headers: {Authorization: `Bearer ${jwtToken}`}});
+    let album;
+    let albums = response.data.results.albums.data;
     //console.log(albums);
+    for (let result of albums) {
+        //console.log(result);
+        if (result.attributes.name == name && result.attributes.artistName == artist) {
+            album = result;
+            break;
+        }
+    }
+    console.log(typeof(album));
+    if (album === undefined) throw new Error(`Error finding release date:${artist} - ${name}`);
+
     console.log(`${album.attributes.name} ---- ${album.attributes.releaseDate}`);
 }
 
-testingArray = [['Charli XCX',  "how i'm feeling now"], ['Khruangbin', 'Mordechai'], 
-['HAIM', 'Women In Music Pt. III'], ['Weyes Blood', 'Titanic Rising'], ['Perfume Genius', 'Set my heart on fire immediately'], 
-['Phoebe Bridgers', 'Punisher'], ['Lady Gaga', 'Chromatica'], ['Yves Tumor', 'Heaven to a tortured mind'], 
-['Moses Sumney', 'grae'], ['Rina Sawayama', 'Sawayama'], ['TOPS', 'I feel alive'],
-['The 1975', 'Notes on a conditional form']];
-
-testingArray.map( album => {
-    getAlbumReleaseDate(album[0], album[1]);
-})
